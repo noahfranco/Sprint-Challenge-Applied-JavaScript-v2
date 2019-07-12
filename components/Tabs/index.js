1b097295892e10adca1be0c3d9513e7cc4c24760
noahfranco <noahfra@gmail.com>
2019-07-12 12:18:55 -0400

    working on Tabs index.js file

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,10 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(data => {
+    // Header(data)
+    console.log("New Topics", data)
+}); 
